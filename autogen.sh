32a5874d8cda6ee02e0476cac99eb0af25c70e90
Dale Whittaker <dayul@users.sf.net>
2007-08-11 11:05:21 +0000

    problem with missing mkinstalldirs
    git-svn-id: https://mailtc.svn.sourceforge.net/svnroot/mailtc@133 cf180f2d-a311-0410-aa27-c105d352ad35

--- autogen.sh
+++ autogen.sh
@@ -2,8 +2,12 @@
 
 echo "Generating configure files... may take a while."
 
-autoreconf --install --force && \
-  echo "Preparing was successful if there was no error messages above." && \
-  echo "Now type:" && \
-  echo "  ./configure && make"  && \
-  echo "Run './configure --help' for more information"
+autoreconf --install --force
+
+#added glib-gettextize as workaround for missing mkinstalldirs file
+glib-gettextize --copy --force
+
+echo "Preparing was successful if there was no error messages above." && \
+echo "Now type:" && \
+echo "  ./configure && make"  && \
+echo "Run './configure --help' for more information"
